36530088517b1289491a2646e16429363c8cc2cc
ebiiim <mail@ebiiim.com>
2018-02-12 03:53:01 +0900

    カラーと白黒の2種類を出力するようにした

--- clipping/csv2wc.py
+++ clipping/csv2wc.py
@@ -14,22 +14,29 @@ def conv_csv_wctext(csv_path):
 
 def randomize(text):
     l = text.split(' ')
-    return ' '.join(random.sample(l,len(l)))
+    return ' '.join(random.sample(l, len(l)))
 
 
-def generate_wordcloud(text, save_path, font_path=None):
+def generate_wordcloud(text, font_path=None):
+    return WordCloud(background_color='white', width=590, height=700,
+                     font_path=font_path, max_font_size=None,
+                     regexp=r"\w[\w'|()._-]+"  # include symbols
+                     ).generate(text)
 
-    WordCloud(background_color='white', width=590, height=700,
-              font_path=font_path, max_font_size=None,
-              regexp=r"\w[\w'|()._-]+"  # include symbols
-              ).generate(text).to_file(save_path)
+
+def mono_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % 0  # monochrome
 
 
 if __name__ == '__main__':
     font_path = '/System/Library/Fonts/ヒラギノ角ゴシック W3.ttc'  # macOS
     csv_path = './wordlist.csv'
-    output_path = './clipping.png'
+    color_output_path = './clipping_color.png'
+    mono_output_path = './clipping_mono.png'
 
     text = randomize(conv_csv_wctext(csv_path))
-    generate_wordcloud(text, output_path, font_path)
-
+    wc = generate_wordcloud(text, font_path)
+    wc.to_file(color_output_path)
+    wc = wc.recolor(color_func=mono_color_func)
+    wc.to_file(mono_output_path)
